24635baf30a2dae18262e75081c23e8291ab9b21
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-07-28 00:41:43 -0300

    feat(tests): Add basic test config and example with pytest

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # ToDo Grupo 2
 
 ## Distribución de ramas
-main: producción
-develop: desarrollo
-ramas por feature: por ejemplo: feat/endpoints
-                                feat/database
-                                feat/tests
+- **main**: producción
+- **develop**: desarrollo
+
 
 ## Levantar el proyecto localmente
-```
+```py
 # Clonar proyecto
 git clone https://github.com/PythonDevTools/ToDo-G2.git
 cd ToDo-G2
@@ -27,4 +25,9 @@ cd ToDo-G2
 
 # Levantamos el proyecto con Uvicorn
 >> uvicorn main:app --reload
+```
+
+## Tests
+```py
+>> pytest
 ```
--- main.py
+++ main.py
@@ -20,20 +20,20 @@ def get_db():
 
 # Route to get all the todo's, it has 2 query params, a skip and a limit
 @app.get("/")
-def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     todos = crud.get_todos(db, skip=skip, limit=limit)
     return todos
 
 
 # Route to create a new todo, it requires a todo in request body
 @app.post("/")
-def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+async def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     db_todo = crud.create_todo(db, todo)
     return db_todo
 
 
 # Route to change the done named bool
 @app.put("/{id}")
-def update_todo(id: int, done: bool = True, db: Session = Depends(get_db)):
+async def update_todo(id: int, done: bool = True, db: Session = Depends(get_db)):
     db_todo = crud.update_todo(db, todo_id=id, done=done)
     return db_todo
