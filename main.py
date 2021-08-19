5ddbc98e0edbd5e8bc02215b754cda9eba5d85b1
Javier Rodriguez <javicerodriguez@gmail.com>
2021-08-19 00:56:26 -0300

    hotfix(reverse change): back 1 commit before

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["*"]
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://todo-fastapi-react.vercel.app"
+]
 
 app.add_middleware(
     CORSMiddleware,
