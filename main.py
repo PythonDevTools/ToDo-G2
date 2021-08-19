a5a19e3dfe0dba9c240ea0284b964250334ebd87
Javier Rodriguez <javicerodriguez@gmail.com>
2021-08-19 00:19:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://todo-fastapi-react.vercel.app/"
+    "https://todo-fastapi-react.vercel.app"
 ]
 
 app.add_middleware(
